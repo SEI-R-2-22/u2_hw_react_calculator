d80cc96ee4cb47b9e3400945df43992dc9642e56
Piero Caceres <pierocaceres92@yahoo.com>
2022-03-17 19:19:54 -0400

    Implemented 4 basic arithmetic

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -4,6 +4,7 @@ function Calculator(props) {
 
     const [firstInput, setFirstInput] = useState('')
     const [secondInput, setSecondInput] = useState('')
+    const [operation, setOperation] = useState('+')
     const [result, setResult] = useState('')
 
     const handleNum = (event, numInput) => {
@@ -14,9 +15,25 @@ function Calculator(props) {
         }
     }
 
+    const handleOperation = (event) => {
+        if(event.target.value === '+'){setOperation('+')}
+        else if(event.target.value === '-'){setOperation('-')}
+        else if(event.target.value === '*'){setOperation('*')}
+        else if(event.target.value === '/'){setOperation('/')}
+    }
+
     const handleSolution = () => {
         if(firstInput && secondInput){
-            setResult(parseInt(firstInput) + parseInt(secondInput))
+            if(operation === '+'){
+                setResult(parseInt(firstInput) + parseInt(secondInput))
+            }else if(operation === '-'){
+                setResult(parseInt(firstInput) - parseInt(secondInput))
+            }else if(operation === '*'){
+                setResult(parseInt(firstInput) * parseInt(secondInput))
+            }else if(operation === '/'){
+                setResult(parseInt(firstInput) / parseInt(secondInput))
+            }
+            
             setFirstInput("")
             setSecondInput("")
         }
@@ -26,7 +43,13 @@ function Calculator(props) {
         <div className="container">
             <div className="add">
                 <input type="text" name="firstInput" placeholder="Enter a number" value={firstInput} onChange={(event) => handleNum(event, firstInput)}/>              
-                <span>+</span>               
+                {/* <span>+</span>                */}
+                <select name="operation" onChange={(event) => handleOperation(event)}>
+                    <option value="+">+</option>
+                    <option value="-">-</option>
+                    <option value="*">*</option>
+                    <option value="/">/</option>
+                </select>
                 <input type="text" name="secondInput" placeholder="Enter a number" value={secondInput} onChange={(event) => handleNum(event, secondInput)}/>
                 <button onClick={handleSolution}>=</button>
             </div>
