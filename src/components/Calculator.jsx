6a2c78b4e6a982d43fc82448eda003572dcf1090
Jackson Christopher <kitchen.king.95@gmail.com>
2022-03-17 17:05:24 -0500

    calculator

---
+++ src/components/Calculator.jsx
@@ -0,0 +1,27 @@
+<div className="container">
+  <div className="add">
+    <input type="text" placeholder="Enter a number" />
+    <span>+</span>
+    <input type="text" placeholder="Enter a number" />
+    <button>=</button>
+  </div>
+  <div className="subtract">
+    <input type="text" placeholder="Enter a number" />
+    <span>-</span>
+    <input type="text" placeholder="Enter a number" />
+    <button>=</button>
+  </div>
+    <div className="multiply">
+    <input type="text" placeholder="Enter a number" />
+    <span>*</span>
+    <input type="text" placeholder="Enter a number" />
+    <button>=</button>
+  </div>
+    <div className="divide">
+    <input type="text" placeholder="Enter a number" />
+    <span>/</span>
+    <input type="text" placeholder="Enter a number" />
+    <button>=</button>
+  </div>
+  <h3 className="results">Results go here!</h3>
+</div>
