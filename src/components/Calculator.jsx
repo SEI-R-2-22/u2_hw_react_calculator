ca92cd880d3f92347bc9001977e1727fe316d7e6
Jenna Leopold <jleopold424@gmail.com>
2022-03-17 18:38:24 -0400

    completed Step 3

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -5,12 +5,32 @@ const Calculator = () => {
   const [num2, setNum2] = useState('')
   const [result, setResult] = useState('')
 
+  const handleNum = (e, num) => {
+    if (num === 'num1') {
+      setNum1(e.target.value)
+    } else {
+      setNum2(e.target.value)
+    }
+  }
+
   return (
     <div className="container">
       <div className="add">
-        <input type="text" placeholder="Enter a number" />
+        <input
+          type="text"
+          name="num1"
+          placeholder="Enter your first number"
+          value={num1}
+          onChange={(e) => handleNum(e, 'num1')}
+        />
         <span>+</span>
-        <input type="text" placeholder="Enter a number" />
+        <input
+          type="text"
+          name="num2"
+          placeholder="Enter your second number"
+          value={num2}
+          onChange={(e) => handleNum(e, 'num2')}
+        />
         <button>=</button>
       </div>
       <h3 className="results">Results go here!</h3>
