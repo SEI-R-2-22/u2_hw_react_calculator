cc0a56e56b030afd5626374a403017017c3e8717
tylusdawkins <tylusjdawkins@gmail.com>
2022-03-17 16:08:32 -0700

    submission

---
+++ src/components/Calculator.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react'
+
+const Calculator = () =>{
+    const [num1, setNum1] = useState('')
+    const [num2, setNum2] = useState('')
+    const [result, setResult] = useState('')
+    const handleNum = (e, num) =>{
+        console.log(num)
+        console.log(e.target.name)
+        console.log(e.target.value)
+        if(e.target.name === "num1"){
+            setNum1(e.target.value)
+        } else if(e.target.name === "num2"){
+            setNum2(e.target.value)
+        }
+    }
+    const handleSolution= () => {
+        if(num1 && num2){
+            setResult(parseInt(num1)+parseInt(num2))
+            console.log(result)
+        }
+    }
+
+    return (
+    <div className="container">
+        <div className="add">
+            <input type="text" name="num1" placeholder="Enter a number" value={num1} onChange={(e) => handleNum(e, num1)}/>
+            <span>+</span>
+            <input type="text" name="num2" placeholder="Enter a number" value={num2} onChange={(e) => handleNum(e, num2)}/>
+            <button onClick={handleSolution}>=</button>
+        </div>
+        <h3 className="results">{result !== '' ? result : 'Solution!'}</h3>
+    </div>
+    )
+}
+
+export default Calculator
