de0535857f1e333fd41ea24235103792c14d1754
Jenna Leopold <jleopold424@gmail.com>
2022-03-17 18:24:55 -0400

    completed Step 2

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,4 +1,10 @@
+import { useState } from 'react';
+
 const Calculator = () => {
+  const [num1, setNum1] = useState('')
+  const [num2, setNum2] = useState('')
+  const [result, setResult] = useState('')
+
   return (
     <div className="container">
       <div className="add">
