552d699d1157923214bec7ae96f3be4351f6aa9d
Piero Caceres <pierocaceres92@yahoo.com>
2022-03-17 19:03:41 -0400

    Updated add functionality

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -6,34 +6,31 @@ function Calculator(props) {
     const [secondInput, setSecondInput] = useState('')
     const [result, setResult] = useState('')
 
-    const handleNum = (e, numInput) => {
-        console.log(numInput)
-        console.log(e.target.value)
-        if(numInput === firstInput){
-            setFirstInput(e.target.value)
-        }else{
-            setSecondInput(e.target.value)
+    const handleNum = (event, numInput) => {
+        if(event.target.name === "firstInput"){
+            setFirstInput(event.target.value)
+        }else if(event.target.name === "secondInput"){
+            setSecondInput(event.target.value)
         }
     }
 
     const handleSolution = () => {
         if(firstInput && secondInput){
             setResult(parseInt(firstInput) + parseInt(secondInput))
+            setFirstInput("")
+            setSecondInput("")
         }
     }
 
     return (
         <div className="container">
             <div className="add">
-                
-                <input type="text" name="firstInput" placeholder="Enter a number" value={firstInput} onChange={(e) => handleNum(e, firstInput)}/>
-                
-                <span>+</span>
-                
-                <input type="text" name="secondInput" placeholder="Enter a number" value={secondInput} onChange={(e) => handleNum(e, secondInput)}/>
+                <input type="text" name="firstInput" placeholder="Enter a number" value={firstInput} onChange={(event) => handleNum(event, firstInput)}/>              
+                <span>+</span>               
+                <input type="text" name="secondInput" placeholder="Enter a number" value={secondInput} onChange={(event) => handleNum(event, secondInput)}/>
                 <button onClick={handleSolution}>=</button>
             </div>
-            <h3 className="results">Results go here!: {result}</h3>
+            <h3 className="results">{result}</h3>
         </div>
     );
 }
