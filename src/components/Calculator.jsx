0ab63bae331d6bf7992e356ebdc02404cb82548d
ryan.ditzel <ryand370@gmail.com>
2022-03-18 08:22:27 -0400

    corrected assignment

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -5,12 +5,14 @@ const Calculator = () => {
     const [num1, setNum1] = useState('')
     const [num2, setNum2] = useState('')
     const [result, setResult] = useState('')   
-    const setNum = (e, num) => {
+    
+    const handleNum = (e, num) =>{
         console.log(num)
+        console.log(e.target.name)
         console.log(e.target.value)
-        if (num === num1 && num !== num2){
+        if(e.target.name === "num1"){
             setNum1(e.target.value)
-        } else {
+        } else if(e.target.name === "num2"){
             setNum2(e.target.value)
         }
     }
@@ -26,9 +28,9 @@ const Calculator = () => {
 return (
         <div className="container">
         <div className="add">          
-            <input type="text" placeholder="Enter a number" value={num1} onChange={(e) => setNum(e, num1)}/>
+            <input type="text" placeholder="Enter a number" value={num1} onChange={(e) => handleNum(e, num1)}/>
             <span>+</span>
-            <input type="text" placeholder="Enter a number" value={num2} onChange={(e) => setNum(e, num2)}/>
+            <input type="text" placeholder="Enter a number" value={num2} onChange={(e) => handleNum(e, num2)}/>
             <button onClick={handleSolution}>=</button>
         </div>
         <h3 className="results">{result}</h3>
