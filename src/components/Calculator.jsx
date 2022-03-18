336b929774f32113562a99d01bdf703f21a67952
Mark Harmon <neothej1sec@gmail.com>
2022-03-17 19:09:42 -0500

    step 3 complete

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,13 +1,37 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const Calculator = (props) => {
+    const [result, setResult] = useState("")
+    let num1
+    let num2
+    const handleNum = (e,num) => {
+        // console.log(num)
+        // console.log(e.target.value)
+        if (num === num1){
+            num1=e.target.value
+        }else{
+            num2=e.target.value
+        }
+        console.log(num1)
+        console.log(num2)
+    }
 
     return (
         <div className="container">
             <div className="add">
-                <input type="text" placeholder="Enter a number" />
+                <input type="text" 
+                    placeholder="Enter a number" 
+                    name="num1"
+                    value={result}
+                    onChange={(e) => handleNum(e, 'num1')}
+                />
                 <span>+</span>
-                <input type="text" placeholder="Enter a number" />
+                <input type="text" 
+                    placeholder="Enter a number" 
+                    name="num2"
+                    value={result}
+                    onChange={(e) => handleNum(e, 'num2')}
+                />
                 <button>=</button>
             </div>
             <h3 className="results">Results go here!</h3>
