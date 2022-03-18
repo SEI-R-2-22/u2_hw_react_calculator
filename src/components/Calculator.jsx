4b01ebb11e6c3a2c5380c951c2bf7bc0d221153b
Mark Kennedy <kennedymark@hotmail.com>
2022-03-17 20:24:15 -0400

    some bonus

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -5,6 +5,7 @@ const Calculator = () => {
   const [num1, setNum1] = useState("")
   const [num2, setNum2] = useState("")
   const [num3, setNum3] = useState("")
+  const [operator, setOperator] = useState('+')
 
 
     const handleNum = (e, num) => {
@@ -15,13 +16,53 @@ const Calculator = () => {
       }
     }
 
+    const changeOperator = () => {
+      switch(operator) {
+        case '+':
+          setOperator('-')
+          break
+        case '-': 
+          setOperator('*')
+          break
+        case '*':
+          setOperator('/')
+          break
+        default:
+          setOperator('+')
+          break
+      }
+    }
+
     const handleSolution = () => {
 
     if (num1 && num2) {
-      let value = parseInt(num1) + parseInt(num2)
-      setNum3(value)
-      setNum2('')
-      setNum1(value)
+      switch(operator) {
+        case '+':
+          let plus = parseInt(num1) + parseInt(num2)
+          setNum3(plus)
+          setNum2('')
+          setNum1(plus)
+          break
+        case '-': 
+          let minus = parseInt(num1) - parseInt(num2)
+          setNum3(minus)
+          setNum2('')
+          setNum1(minus)
+          break
+        case '*':
+          let mult = parseInt(num1) * parseInt(num2)
+          setNum3(mult)
+          setNum2('')
+          setNum1(mult)
+          break
+        default:
+          let div = parseInt(num1) / parseInt(num2)
+          setNum3(div)
+          setNum2('')
+          setNum1(div)
+          break
+      }
+
     }
 
     }
@@ -31,9 +72,9 @@ const Calculator = () => {
     <div className="container">
       <div className="add">
         <input type="text" placeholder="Enter a number" onChange={(e) => handleNum(e , 'num1')} value={num1}/>
-        <span>+</span>
+        <span onClick={() => changeOperator()}>{operator}</span>
         <input type="text" placeholder="Enter a number" onChange={(e) => handleNum(e , 'num2')} value={num2}/>
-        <button onClick={() => handleSolution()}>=</button>
+        <button onClick={num1 && num2 ? () => handleSolution() : null}>=</button>
       </div>
       <h3 className="results">{num3}</h3>
     </div>
