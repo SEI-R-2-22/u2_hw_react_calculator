3838fb54d305cd19e73c2bd8e69c23ce5715bbf1
Piero Caceres <pierocaceres92@yahoo.com>
2022-03-17 20:05:08 -0400

    Erased commented code.

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -7,37 +7,13 @@ function Calculator(props) {
     const [operation, setOperation] = useState('+')
     const [result, setResult] = useState('')
 
-    const handleNum = (event, numInput) => {
+    const handleNum = (event) => {
         if(event.target.name === "firstInput"){
             setFirstInput(event.target.value)
         }else if(event.target.name === "secondInput"){
             setSecondInput(event.target.value)
         }
     }
-
-    const handleOperation = (event) => {
-        if(event.target.value === '+'){setOperation('+')}
-        else if(event.target.value === '-'){setOperation('-')}
-        else if(event.target.value === '*'){setOperation('*')}
-        else if(event.target.value === '/'){setOperation('/')}
-    }
-
-    // const handleSolution = () => {
-    //     if(firstInput && secondInput){
-    //         if(operation === '+'){
-    //             setResult(parseInt(firstInput) + parseInt(secondInput))
-    //         }else if(operation === '-'){
-    //             setResult(parseInt(firstInput) - parseInt(secondInput))
-    //         }else if(operation === '*'){
-    //             setResult(parseInt(firstInput) * parseInt(secondInput))
-    //         }else if(operation === '/'){
-    //             setResult(parseInt(firstInput) / parseInt(secondInput))
-    //         }
-            
-    //         setFirstInput("")
-    //         setSecondInput("")
-    //     }
-    // }
 
     const handleSolution = () => {
         if(firstInput && secondInput){
@@ -59,14 +35,14 @@ function Calculator(props) {
     return (
         <div className="container">
             <div className="add">
-                <input type="text" name="firstInput" placeholder="Enter a number" value={firstInput} onChange={(event) => handleNum(event, firstInput)}/>              
-                <select name="operation" onChange={(event) => handleOperation(event)}>
+                <input type="text" name="firstInput" placeholder="Enter a number" value={firstInput} onChange={(event) => handleNum(event)}/>              
+                <select name="operation" onChange={(event) => setOperation(event.target.value)}>
                     <option value="+">+</option>
                     <option value="-">-</option>
                     <option value="*">*</option>
                     <option value="/">/</option>
                 </select>
-                <input type="text" name="secondInput" placeholder="Enter a number" value={secondInput} onChange={(event) => handleNum(event, secondInput)}/>
+                <input type="text" name="secondInput" placeholder="Enter a number" value={secondInput} onChange={(event) => handleNum(event)}/>
                 <button onClick={handleSolution}>=</button>
             </div>
             <h3 className="results">{handleSolution()}</h3>
