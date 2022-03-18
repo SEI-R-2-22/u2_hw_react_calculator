7f6f1d4bc76af78524b6b3da3ad98b2c6fd11a02
Daniel Kuntz <kuntzd99@gmail.com>
2022-03-17 22:00:46 -0500

    Added times button

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -4,6 +4,7 @@ const Calculator = () => {
   const [firstNum, setFirstNum] = useState('')
   const [secondNum, setSecondNum] = useState('')
   const [result, setResult] = useState(0)
+  const [operator, setOperator] = useState('')
 
   const handleFirstNum = (e) => {
     setFirstNum(e.target.value)
@@ -15,16 +16,31 @@ const Calculator = () => {
 
   const handleResult = () => {
     if (firstNum !== '' && secondNum !== '' && !isNaN(parseInt(firstNum)) && !isNaN(parseInt(secondNum))) {
-      let sum = parseInt(firstNum) + parseInt(secondNum)
-      setResult(sum)
+      switch(operator) {
+        case 'sum':
+          setResult(parseInt(firstNum) + parseInt(secondNum))
+          break
+        case 'prod':
+          setResult(parseInt(firstNum) * parseInt(secondNum))
+          break
+      }
     }
+  }
+
+  const handleSum = () => {
+    setOperator('sum')
+  }
+
+  const handleProd = () => {
+    setOperator('prod')
   }
 
   return (
     <div className="container">
       <div className="add">
         <input type="text" placeholder="Enter a number" onChange={handleFirstNum} />
-        <span>+</span>
+        <button onClick={handleSum}>+</button>
+        <button onClick={handleProd}>*</button>
         <input type="text" placeholder="Enter a number" onChange={handleSecondNum} />
         <button onClick={handleResult}>=</button>
       </div>
