0bbe7830b524a3d4e592700bb9432bc9e15ea34e
Frank Marra <frankrmarra@gmail.com>
2022-03-17 20:09:42 -0400

    homework completed with all bonus except for last

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import './App.css'
+import Calculator from './components/Calculator'
+import { useState } from 'react'
 
 const App = () => {
-  
+  const [firstNumber, setFirstNumber] = useState('')
+  const [secondNumber, setSecondNumber] = useState('')
+  const [totalNumber, setTotalNumber] = useState(0)
+  const [sign, setSign] = useState('')
+
   return (
     <div className="App">
       <h1>React Calculator</h1>
 
-      {/* Your <Calculator /> component here */}
+      <Calculator
+        firstNumber={firstNumber}
+        secondNumber={secondNumber}
+        totalNumber={totalNumber}
+        setFirstNumber={setFirstNumber}
+        setSecondNumber={setSecondNumber}
+        setTotalNumber={setTotalNumber}
+        sign={sign}
+        setSign={setSign}
+      />
     </div>
   )
 }
---
+++ src/components/Calculator.jsx
@@ -0,0 +1,85 @@
+const Calculator = ({
+  firstNumber,
+  secondNumber,
+  totalNumber,
+  setFirstNumber,
+  setSecondNumber,
+  setTotalNumber,
+  sign,
+  setSign
+}) => {
+  const handleInput = (e, num) => {
+    if (num === 'First Number') {
+      setFirstNumber(e.target.value)
+    } else if (num === 'Second Number') {
+      setSecondNumber(e.target.value)
+    } else if (num === 'Math Operator') {
+      setSign(e.target.value)
+    }
+  }
+  const handleSolution = () => {
+    if (isNaN(firstNumber) || isNaN(secondNumber)) {
+      setTotalNumber('Please enter two numbers')
+    } else if (isNaN(firstNumber) === false && isNaN(secondNumber) === false) {
+      switch (sign) {
+        case '+':
+          setTotalNumber(parseInt(firstNumber) + parseInt(secondNumber))
+          setFirstNumber('')
+          setSecondNumber('')
+          setSign('')
+          break
+        case '-':
+          setTotalNumber(parseInt(firstNumber) - parseInt(secondNumber))
+          setFirstNumber('')
+          setSecondNumber('')
+          setSign('')
+          break
+        case '*':
+          setTotalNumber(parseInt(firstNumber) * parseInt(secondNumber))
+          setFirstNumber('')
+          setSecondNumber('')
+          setSign('')
+          break
+        case '/':
+          setTotalNumber(parseInt(firstNumber) / parseInt(secondNumber))
+          setFirstNumber('')
+          setSecondNumber('')
+          setSign('')
+          break
+        default:
+          setTotalNumber('Please enter +, -, *, / as an operator')
+      }
+    }
+  }
+
+  return (
+    <div className="container">
+      <div className="add">
+        <input
+          type="text"
+          placeholder="Number"
+          value={firstNumber}
+          onChange={(e) => handleInput(e, 'First Number')}
+        />
+        <span>
+          <input
+            type="text"
+            placeholder="Operator"
+            value={sign}
+            onChange={(e) => handleInput(e, 'Math Operator')}
+          />
+        </span>
+        <input
+          type="text"
+          placeholder="Number"
+          value={secondNumber}
+          onChange={(e) => handleInput(e, 'Second Number')}
+        />
+        <button onClick={() => handleSolution()}>=</button>
+      </div>
+      <h3 className="results">{totalNumber}</h3>
+    </div>
+  )
+}
+
+export default Calculator
