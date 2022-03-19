c51e1a833f63bc8c5310d839194a0c2321f1f57d
Mark Ryan <markrryan99@gmail.com>
2022-03-19 15:38:59 -0700

    calculator functional with addition

---
+++ src/components/Calculator.jsx
@@ -0,0 +1,51 @@
+import { useState } from 'react'
+
+const Calculator = () => {
+
+    let [num1, setNum1] = useState('')
+    let [num2, setNum2] = useState('')
+    let [results, setResults] = useState('')
+
+    const handleNum = (e, num) => {
+        console.log(num)
+        console.log(e.target.value)
+
+        if (num === 'num1') {
+            setNum1(e.target.value)
+        } else {
+            setNum2(e.target.value)
+        }
+    }
+
+    const handleSolution = () => {
+        if (num1 !== '' && num2 !== '') {
+            setResults(parseInt(num1) + parseInt(num2))
+        }
+    }
+
+    return (
+        <div className="container">
+            <div className="add">
+            <input
+                type="number"
+                name="num1"
+                placeholder="Enter your first number"
+                // value={someStateVariable}
+                onChange={(e) => handleNum(e, 'num1')}
+            />
+                <span>+</span>
+            <input
+                type="number"
+                name="num2"
+                placeholder="Enter your second number"
+                // value={someStateVariable}
+                onChange={(e) => handleNum(e, 'num2')}
+            />
+                <button onClick={() => handleSolution()}>=</button>
+            </div>
+            <h3 className="results">{results}</h3>
+        </div>
+    )
+}
+
+export default Calculator
