51a79995fee264d1105525e51ee6ecacec6d7b7c
Ifeoluwa Adebisi <adebisiife@gmail.com>
2022-03-20 23:55:24 -0400

    completed

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,21 +1,52 @@
+import { useState } from 'react'
 
 const Calculator = () => {
+  const [num1, setNumber1] = useState('')
+  const [num2, setNumber2] = useState('')
+  const [result, setResult] = useState('Results go here!')
+
+  const handleNum = (e, num) => {
+    console.log(num)
+    console.log(e.target.value)
+    setNumber1(e.target.value)
+  }
+  const handleNum1 = (e, num) => {
+    console.log(num)
+    console.log(e.target.value)
+    setNumber2(e.target.value)
+  }
+   const handleSolution = () => {
+    const result = parseInt(num1) + parseInt(num2)
+    if (isNaN(result)) {
+      alert('Not a number')
+    }
+    setResult(result)
+    setNumber1('');
+    setNumber2('');
+   }
+
     return(
         <div className="container">
         <div className="add">
-          <input type="text" placeholder="Enter a number" />
-          <span>+</span>
-          <input type="text" placeholder="Enter a number" />
-          <button>=</button>
           <input
-            type="number"
+            type="text"
             name="num1"
-            placeholder="Enter your first number"
-            value={someStateVariable}
+            placeholder="Enter a number"
+            value={num1}
             onChange={(e) => handleNum(e, 'num1')}
             />
+          <span>+</span>
+          <input
+            type="text"
+            name="num2"
+            placeholder="Enter a number"
+            value={num2}
+            onChange={(e) => handleNum1(e, 'num2')}
+            />
+          <button onClick={() => handleSolution()}>=</button>
+          
         </div>
-        <h3 className="results">Results go here!</h3>
+        <h3 className="results">{ result }</h3>
       </div> 
     ) 
 }
