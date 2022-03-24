92a6209dee7aa48955c0d6f7f2f24dd6701edbcc
Abdullah Albayati <dark.king1154@gmail.com>
2022-03-23 19:42:38 -0500

    Added the logic to add 2 nums

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
+
 import './App.css'
+import Calculator from './components/Calculator'
+import { useState } from 'react'
 
 const App = () => {
-  
+
+  const[num1, setNum1] = useState('')
+  const[num2, setNum2] = useState('')
+  const[operator, setOperator] = useState('')
+  const[result, setResult] = useState('')
+
   return (
     <div className="App">
       <h1>React Calculator</h1>
 
-      {/* Your <Calculator /> component here */}
+      <Calculator
+          num1 = { num1 }
+          setNum1 = { setNum1 }
+          num2 = { num2 }
+          setNum2 = { setNum2 }
+          operator = { operator }
+          setOperator = { setOperator }
+          result = { result }
+          setResult = { setResult }
+      />
     </div>
   )
 }
-
-export default App
